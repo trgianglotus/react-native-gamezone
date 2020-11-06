192f81c2908a7f4ba0f51a18846161192c16d5e2
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-11-06 21:35:37 +0800

    set up screens

---
+++ screens/ReviewDetails.js
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ReviewDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>ReviewDetails Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 24 },
+});
