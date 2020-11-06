2ca49b84c976c1cd8abd900e0c253b5e4659e28e
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-11-06 23:20:40 +0800

    add card

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/Card';
 
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
@@ -32,7 +33,9 @@ export default function Home({ navigation }) {
           <TouchableOpacity
             onPress={() => navigation.navigate('ReviewDetails', item)}
           >
-            <Text style={globalStyles.titleText}>{item.title}</Text>
+            <Card>
+              <Text style={globalStyles.titleText}>{item.title}</Text>
+            </Card>
           </TouchableOpacity>
         )}
       />
